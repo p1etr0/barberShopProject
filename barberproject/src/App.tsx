d3fa3d0c5234861fc42e2f1fd6152d37bef20e5c
Pietro Gonçalves <pietro_goncalves@hotmail.com>
2021-10-04 21:38:14 -0300

    Backend

--- barberproject/src/App.tsx
+++ barberproject/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  private api = axios.create({
+    baseURL: "http://localhost:3333/"
+  })
+
+  state = {
+    message: '',
+  }
+
+  async componentDidMount(){
+    const response = await this.api.get("/");
+    this.setState(response.data, () => {});
+  }
+  
+
+  render(){
+    const { message } = this.state;
+    console.log(message);
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            {message} teste
+          </p>
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- barberprojectbackend/src/server.ts
+++ barberprojectbackend/src/server.ts
@@ -5,7 +5,12 @@ const app = express();
 app.use(express.json())
 
 app.get("/", (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
   return response.json({
-    message: "Hello World"
+    message: "PIETRO"
   })
+})
+
+app.listen(3333, () => {
+  console.log("Server started!")
 })
